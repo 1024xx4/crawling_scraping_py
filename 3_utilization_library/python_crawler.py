331e2b4f7e09706836b29a1bed3493019670d1f8
1024xx4 <teraleaf1024.4@gmail.com>
2021-12-14 07:01:13 +0900

    Feature: 詳細 Page から scraping する
    3.13

--- 3_utilization_library/python_crawler.py
+++ 3_utilization_library/python_crawler.py
@@ -7,14 +7,19 @@ from typing import Iterator
 import requests
 import lxml.html
 
+
 def main():
     """
     Crawler の main 処理
     """
+    session = requests.Session()
     response = requests.get('https://gihyo.jp/dp')
     urls = scrape_list_page(response)
     for url in urls:
-        print(url)
+        response = session.get(url)
+        ebook = scrape_detail_page(response)
+        print(ebook)
+        break
 
 
 def scrape_list_page(response: requests.Response) -> Iterator[str]:
@@ -29,6 +34,23 @@ def scrape_list_page(response: requests.Response) -> Iterator[str]:
     for a in html.cssselect('#listBook > li > a[itemprop="url"]'):
         url = a.get('href')
         yield url
+
+
+def scrape_detail_page(response: requests.Response) -> dict:
+    """
+    詳細 Page の Response から電子書籍の情報を dict で取得する。
+    :param response:
+    :return: dict
+    """
+    html = lxml.html.fromstring(response.text)
+    ebook = {
+        'url': response.url,
+        'title': html.cssselect('#bookTitle')[0].text_content(),
+        'price': html.cssselect('.buy')[0].text,
+        'content': [h3.text_content() for h3 in html.cssselect('#content > h3')],
+    }
+    return ebook
+
 
 if __name__ == '__main__':
     main()
