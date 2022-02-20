de736cc79421b7b2edab8221cd9df07b3753d19d
1024xx4 <teraleaf1024.4@gmail.com>
2022-02-21 05:36:04 +0900

    Practice: zyte の Blog から投稿の Title を取得する
    Spider の試行

---
+++ items.jl
@@ -0,0 +1,11 @@
+{"title": "Zyte named as one of Deloitte Technology Fast 50"}
+{"title": "Zyte named as one of Deloitte Technology Fast 50"}
+{"title": "How to extract data from an HTML table"}
+{"title": "What is a proxy server and how do they work?"}
+{"title": "Extract Summit 2021: Highlights and key takeaways"}
+{"title": "How does a headless browser help with web scraping and data extraction?"}
+{"title": "Proxies versus VPNs: What\u2019s the difference, and which one is right for my use case?"}
+{"title": "Manage bans and get your data with Zyte Data API Smart Browser"}
+{"title": "How to reduce noise in the data through data parsing"}
+{"title": "What is web data harvesting?"}
+{"title": "In pursuit of perfection: measuring web product data quality"}
