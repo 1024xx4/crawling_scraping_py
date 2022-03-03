c917f41d8b859ee066d5a38770195430068dc498
1024xx4 <teraleaf1024.4@gmail.com>
2022-03-04 06:58:26 +0900

    Feat: Item を検証する Pipeline を実装する#13

--- myproject/myproject/pipelines.py
+++ myproject/myproject/pipelines.py
@@ -6,8 +6,23 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class MyprojectPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class ValidationPipeline:
+    """
+    Item を検証する Pipeline
+    """
+
+    def process_item(self, item, spider):
+        if not item['title']:
+            # title Field が取得できていない場合は破棄する。
+            # DropItem() の引数は破棄する理由を表す Message.
+            raise DropItem('Missing title')
+
+        return item  # title Field が正しく取得できている場合。
